43d5bbc6d3cc3526c9e3a07f24207e2cafc2e94c
Keith Williams (willik01) <me@keithwilliams.net>
2022-10-05 12:44:34 -0700

    Added destinations for flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -4,10 +4,11 @@ module.exports = {
     new: newFlight,
     create, 
     index,
+    show,
 }
 //new flight create page
 function newFlight(req, res) {
-    res.render('flights/new');
+    res.render('flights/new', {title: 'Add New Flight'});
 }
 
 // add new flight to db
@@ -30,9 +31,24 @@ function create(req, res) {
 //show all flights
 function index(req, res) {
     Flight.find({}, function(err, flights) {
-        console.log(flights)
+        // console.log(flights)
         res.render('flights/index', {
-            flights 
+            flights, 
+            title: 'All Flights' 
         })
+    })
+}
+
+//show flight details
+function show(req, res) {
+    const test = Flight.find({})
+    console.log(test)
+    Flight.findById(req.params.id, function(err, flight) {
+        //attempting to sort destinations
+        // flight.destinations.sort('arrival')
+        // Flight.destinations.find({}).sort('arrival').exec((err, flight)=> {console.log('flight destinations: ', flight.destinations)});
+        // Room.find({}).sort('-date').exec((err, docs) => { ... });
+
+        res.render('flights/show', {title: 'Flight Detail', flight});
     })
 }
